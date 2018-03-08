46a2f057eb405881d6bfff78ce9333ed6c25f999
vkadimisetty <vkadimisetty>
2018-03-08 11:08:17 +0530

    enabled cors

--- app.js
+++ app.js
@@ -55,10 +55,17 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(function(req, res, next) {
+      res.set('Access-Control-Allow-Origin', '*');
+      res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
+      res.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Accept,Authorization,x-access-token');
+      next();
+  });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 //app.use('/users',VerifyToken);
